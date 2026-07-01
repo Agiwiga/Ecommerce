02e434e59dc6204c14bcd3d8d8c2afad3d0bfa98
Agiw <4g1lk115@gmail.com>
2026-07-01 19:32:10 +0700

    User detail product

--- app/src/main/java/com/example/ecommerce/adapter/ProductAdapter.kt
+++ app/src/main/java/com/example/ecommerce/adapter/ProductAdapter.kt
@@ -33,17 +33,35 @@ class ProductAdapter(
     inner class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val nameTextView: TextView = itemView.findViewById(R.id.textViewProductName)
         private val priceTextView: TextView = itemView.findViewById(R.id.textViewProductPrice)
-        private val descriptionTextView: TextView =
-            itemView.findViewById(R.id.textViewProductDescription)
+        private val descriptionTextView: TextView = itemView.findViewById(R.id.textViewProductDescription)
+        private val categoryTextView: TextView = itemView.findViewById(R.id.textViewProductCategory)
+        private val unitTextView: TextView = itemView.findViewById(R.id.textViewProductUnit)
+        private val stockTextView: TextView = itemView.findViewById(R.id.textViewProductStock)
         private val detailButton: Button = itemView.findViewById(R.id.buttonViewDetail)
+
 
         fun bind(product: Product) {
             nameTextView.text = product.name
-            priceTextView.text = formatPrice(product.price)
+            priceTextView.text =
+                if (product.saleType == "Berat")
+                    "${formatPrice(product.price)} / Kg"
+                else
+                    "${formatPrice(product.price)} / Pcs"
+
+            categoryTextView.text = "Kategori : ${product.category}"
+            if (product.saleType == "Berat") {
+                unitTextView.text = "Isi Karung : ${product.packageQuantity} Kg"
+                stockTextView.text = "Stok : ${product.stock.toInt()} Karung"
+            } else {
+                unitTextView.text = "Isi Pack : ${product.packageQuantity.toInt()} Pcs"
+                stockTextView.text = "Stok : ${product.stock.toInt()} Pack"
+            }
+
             descriptionTextView.text = product.description
             detailButton.setOnClickListener {
                 onDetailClick(product)
             }
+
         }
     }
 
